f102645188d1609d48742caf0fb2b3060e765f2d
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-09-26 17:15:30 -0500

    Parse function

--- Logic/lexer.py
+++ Logic/lexer.py
@@ -3,11 +3,19 @@ sys.path.append("..")
 
 from Logic.Types import typeR as R
 
-def verify(instructions):
-  ##Determine if input's correct
+def parse(instructions):
+  ##Split each input line
   instructions = instructions.strip().split("\n")
 
+  ##Split each line and delete special characters
   instructions = list(map(lambda x : x.replace(",", " ").replace("(", " ").replace(")", " ").split(), instructions))
+  return instructions
+
+def verify(instructions):
+  ##Determine if input's correct
+
+  ##Parsing input
+  instructions = parse(instructions)
 
   for instruction in instructions:
     print(instruction)
