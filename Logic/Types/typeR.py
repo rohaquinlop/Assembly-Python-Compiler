bed5ce35d0f24402199763ceab546945e567bfca
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-09-26 19:14:33 -0500

    Created Functions

--- Logic/Types/typeR.py
+++ Logic/Types/typeR.py
@@ -1,15 +1,20 @@
+def getFunctionCode(function):
+  functionCode = {
+    "add"   : "100000",
+    "addu"  : "100001",
+    "sub"   : "100010",
+    "subu"  : "100011",
+    "and"   : "100100",
+    "or"    : "100101",
+    "nor"   : "100111",
+    "slt"   : "101010",
+    "sltu"  : "101011",
+    "sll"   : "000000",
+    "srl"   : "000010",
+    "jr"    : "001000",
+  }
+  return functionCode[function]
 
-functionCode = {
-  "add"   : "100000",
-  "addu"  : "100001",
-  "sub"   : "100010",
-  "subu"  : "100011",
-  "and"   : "100100",
-  "or"    : "100101",
-  "nor"   : "100111",
-  "slt"   : "101010",
-  "sltu"  : "101011",
-  "sll"   : "000000",
-  "srl"   : "000010",
-  "jr"    : "001000",
-}
+def isFunction(function):
+  functions = ["add", "addu", "sub", "subu", "and", "or", "nor", "slt", "sltu", "sll", "srl", "jr"]
+  return function in functions
