5ef8c6dba28cac9cd177e08a9d93fea9c067ff87
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-09-27 16:27:49 -0500

    R instruction validation

--- Logic/lexer.py
+++ Logic/lexer.py
@@ -4,6 +4,7 @@ sys.path.append("..")
 from Logic.Types import typeR as R
 from Logic.Types import typeI as I
 from Logic.Types import typeJ as J
+from Logic.Types import registers as rgstr
 
 def parse(instructions):
   """
@@ -15,11 +16,46 @@ def parse(instructions):
   instructions = list(map(lambda x : x.replace(",", " ").replace("(", " ").replace(")", " ").split(), instructions))
   return instructions
 
+def isRInstruction(instruction):
+  ##Validate if there is the correct length for the instruction
+  if n == 4:
+    ##Is a valid length
+    for i in range(1, n):
+      if i == 1 and instruction[i] == "$zero":
+        ## If we are trying to modify the register $zero then return False, it's not allowed
+        return False
+      else:
+        if !rgstr.isRegister(instruction[i]) or rgstr.isReserved(instruction[i]):
+          ##If it's not a valid register or it's a reserved register then return False, it's not allowed
+          return False
+    ##If finalizes all the process that means that the input was correct (Never returned False)
+    return True
+  elif n == 2:
+    ##Is a valid length (jr)
+    if instruction[0] == "jr" and rgstr.isRegister(instruction[1]):
+      ##Means that the instruction is correct
+      return True
+    else:
+      return False
+  else:
+    return False
+
 def isAcceptable(instruction):
   """
   Determine if a instruction is acceptable or not
   return True or False
   """
+  n = len(instruction)
+  if R.isFunction(instruction[0]):
+    ##Is a type R instruction
+    return isRInstruction(instruction)
+  elif I.isOpCode(instruction[0]):
+    ##Is a type I instruction
+  elif J.isOpCode(instruction[0]):
+    ##Is a type J instruction
+  else:
+    ##It's a not valid case
+    return False
 
 def verify(instructions):
   """
