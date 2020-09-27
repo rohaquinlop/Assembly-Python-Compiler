7d8c07acfcabc180822828bcc3e5ff04559dfde2
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-09-26 23:33:37 -0500

    Modified documentation
    Added some details in the function's documentation and added the function isAcceptable canvas, isn't complete yet

--- Logic/lexer.py
+++ Logic/lexer.py
@@ -2,17 +2,30 @@ import sys
 sys.path.append("..")
 
 from Logic.Types import typeR as R
+from Logic.Types import typeI as I
+from Logic.Types import typeJ as J
 
 def parse(instructions):
-  ##Split each input line
+  """
+  Split each input line and return it
+  """
   instructions = instructions.strip().split("\n")
 
   ##Split each line and delete special characters
   instructions = list(map(lambda x : x.replace(",", " ").replace("(", " ").replace(")", " ").split(), instructions))
   return instructions
 
+def isAcceptable(instruction):
+  """
+  Determine if a instruction is acceptable or not
+  return True or False
+  """
+
 def verify(instructions):
-  ##Determine if input's correct
+  """
+  Determine if input's correct
+  return True or False
+  """
 
   ##Parsing input
   instructions = parse(instructions)
