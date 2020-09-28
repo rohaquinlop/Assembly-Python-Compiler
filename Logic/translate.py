6d4e7b97e383c823857823638312e5569e38edc0
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-09-28 07:30:20 -0500

    Create function convert inmediate to binary

--- Logic/translate.py
+++ Logic/translate.py
@@ -7,6 +7,16 @@ from Logic.Types import typeJ as J
 from Logic.Types import registers as rgstr
 from Logic import lexer as lx
 
+def convertInmediate(inmediate):
+  res = ""
+  if 'x' in inmediate:
+    res += "{0:05b}".format(int(inmediate[inmediate.find('x')+1:], 16))
+  elif 'X' in inmediate:
+    res += "{0:05b}".format(int(inmediate[inmediate.find('X')+1:], 16))
+  else:
+    res += "{0:05b}".format(inmediate)
+  return res
+
 
 def translate(instructions):
   instructions = lx.parse(instructions)
