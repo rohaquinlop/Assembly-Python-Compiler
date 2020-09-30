16f68c189737885d8126ae285bf30ff902a257dd
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-09-30 02:47:19 -0500

    Update translate.py

--- Logic/translate.py
+++ Logic/translate.py
@@ -68,7 +68,7 @@ def getTranslationR(instruction):
   elif instruction[0] in ["add", "addu", "sub", "subu", "and", "or", "nor", "slt", "sltu"]:
     return (R.getOpCode(instruction[0]) + rgstr.getRegisterCode(instruction[2]) + rgstr.getRegisterCode(instruction[3]) + rgstr.getRegisterCode(instruction[1]) + "00000" + R.getFunctionCode(instruction[0]))
   elif instruction[0] in ["div", "divu", "mult", "multu"]:
-    return (R.getOpCode(instruction[0]) + rgstr.getRegisterCode(instruction[2]) + rgstr.getRegisterCode(instruction[3]) + "00000" + "00000" + R.getFunctionCode(instruction[0]))
+    return (R.getOpCode(instruction[0]) + rgstr.getRegisterCode(instruction[2]) + rgstr.getRegisterCode(instruction[1]) + "00000" + "00000" + R.getFunctionCode(instruction[0]))
   elif instruction[0] == "jr":
     return (R.getOpCode(instruction[0]) + rgstr.getRegisterCode(instruction[1]) + "00000" + "00000" + "00000" + R.getFunctionCode(instruction[0]))
   elif instruction[0] in ["mfhi", "mflo"]:
@@ -113,8 +113,9 @@ def translate(instructions):
   finalInstructions = ""
   PC = 0
   for instruction in instructions:
-    PC += 4
-    ans  = getTranslation(instruction, tagsPos, PC)
-    if len(ans) > 0:
-      finalInstructions += "{0}\n".format(ans)
+    if len(instruction) > 0:
+      PC += 4
+      ans  = getTranslation(instruction, tagsPos, PC)
+      if len(ans) > 0:
+        finalInstructions += "{0}\n".format(ans)
   return finalInstructions.strip()
