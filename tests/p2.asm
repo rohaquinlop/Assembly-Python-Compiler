37850870bb0b0eb65f8b1961f3b3f7b0dd576b6c
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-09-30 02:44:18 -0500

    Update p2.asm

--- tests/p2.asm
+++ tests/p2.asm
@@ -1,20 +1,20 @@
 factorial:
-    addi $sp,$sp,-8   #reservo 8
-    sw $a0, 4($sp)         #guardo parametro de enrtrada
-    sw $ra, 0($sp)        #guardo valor de retorno
+    addi $sp,$sp,-8   
+    sw $a0, 4($sp)         
+    sw $ra, 0($sp)        
     addi $t0,$0,2
-    slt $t0,$a0,$t0        #pregunto si levalor de entrada es menor que 2
-    beq $t0, $0, else       # si no es menor osea que no es 1, va al else
-    addi $v0,$0,1        #ultima entrada, le suma 1
-    addi $sp ,$sp , 8    #libera direccion
-    jr $ra            #salta al anterior
+    slt $t0,$a0,$t0        
+    beq $t0, $0, else       
+    addi $v0,$0,1        
+    addi $sp ,$sp , 8    
+    jr $ra            
 
 
 else:
-    addi $a0,$a0,-1        #le resto 1 a n
-    jal factorial        #salta a factorial y deja un nuevo ra
-    lw $ra, 0($sp)        #recupera direccion anterior
-    lw $a0 , 4($sp)        #recupera el valor anterior
-    addi $sp, $sp, 8        #desapila
-    mul $v0, $a0, $v0    #multiplica
-    jr $ra            #salta al ra
+    addi $a0,$a0,-1        
+    jal factorial        
+    lw $ra, 0($sp)        
+    lw $a0 , 4($sp)        
+    addi $sp, $sp, 8        
+    mult $v0, $a0
+    jr $ra            
